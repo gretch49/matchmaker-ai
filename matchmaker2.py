56fc2f24873528459006583ea3a778ca9f05a254
gretch49 <160289596+gretch49@users.noreply.github.com>
2024-04-15 12:16:07 -0400

    Update matchmaker2.py

--- matchmaker2.py
+++ matchmaker2.py
@@ -197,9 +197,7 @@ if __name__ == "__main__":
         get_keywords_button = st.button('Get keywords', on_click=clicked, args=["key_button_get_keywords"],type="primary")
 
         if job_description:
-            "hi"
-        if st.session_state.clicked["key_button_get_keywords"]:
-            "hello world"
+            get_keywords_button
 
 
         if st.session_state.clicked["key_button_get_keywords"] and not job_description:
